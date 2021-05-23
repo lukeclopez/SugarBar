cd5f7d27636aa9d89855446237d954715d693c58
Luke Lopez <xlucas14@gmail.com>
2021-05-22 22:16:56 -0400

    Renamed, rearranged, refined for better sep of concerns

--- sugarbar.1m.py
+++ sugarbar.1m.py
@@ -35,12 +35,12 @@ DIRECTIONS = {
 }
 
 def refresh():
-    sugar_mgdl, direction = get_reading()
+    sugar_mgdl, direction = get_reading_data()
     display(sugar_mgdl, direction)
 
 # Helpers
 
-def get_reading():
+def get_reading_data():
     res = requests.get(BASE_URL + ENDPOINT + TOKEN)
 
     if res.status_code != 200:
@@ -50,18 +50,20 @@ def get_reading():
     sugar_mgdl = get_sugar_level(data)
     direction = get_direction(data)
     return (sugar_mgdl, direction)
-    
-
-def display(sugar_mgdl, direction):
-    display = f"{EMOJI_DROP_OF_BLOOD} {sugar_mgdl} {direction}"
-    display_string = f"{display} | href={BASE_URL} | color={get_color(sugar_mgdl)}"
-    print(display_string)
 
 def get_sugar_level(data):
     return str(data["sgv"])
 
 def get_direction(data):
-    return DIRECTIONS.get(data["direction"], "E")
+    return data.get("direction")
+
+def display(sugar_mgdl, direction):
+    display = f"{EMOJI_DROP_OF_BLOOD} {sugar_mgdl} {get_direction_indicator(direction)}"
+    display_string = f"{display} | href={BASE_URL} | color={get_color(sugar_mgdl)}"
+    print(display_string)
+
+def get_direction_indicator(direction):
+    return DIRECTIONS.get(direction, "E")
 
 def get_color(sugar_mgdl):
     return "blue"
