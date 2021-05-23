e90eed86527edf45750aed9aebd24bc38a602eae
Luke Lopez <xlucas14@gmail.com>
2021-05-22 20:11:43 -0400

    First commit

---
+++ sugarbar.1m.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding=utf-8
+#
+# <xbar.title>Dexcom Blood Sugar</xbar.title>
+# <xbar.version>v1.0</xbar.version>
+# <xbar.author>lukeclopez</xbar.author>
+# <xbar.author.github>lukeclopez</xbar.author.github>
+# <xbar.desc>Displays my blood sugar from night scout</xbar.desc>
+# <xbar.image>https://i.imgur.com/I3MdNmU.png</xbar.image>
+# <xbar.dependencies>python</xbar.dependencies>
+#
+# by lukeclopez
+
+
+print("Hey you")
